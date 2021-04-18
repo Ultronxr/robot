98b4819961a189ab3ca129004699c99b3245910f
ultronxr <Ultronxr@users.noreply.github.com>
2021-04-18 10:11:23 +0800

    [remote] 1. 移除redisson依赖（目前用不到redis） 2. 版本迭代 version:1.2.3

--- remote/src/main/java/cn/ultronxr/remote/config/RedisConfig.java
+++ remote/src/main/java/cn/ultronxr/remote/config/RedisConfig.java
@@ -1,38 +1,38 @@
-package cn.ultronxr.remote.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.net.UnknownHostException;
-
-
-@Configuration
-public class RedisConfig {
-
-    /**
-     * 修改 RedisTemplate 配置
-     */
-    @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory)
-            throws UnknownHostException {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(factory);
-
-        // 修改 RedisTemplate K-V序列化方式，避免存入redis时出现乱码（\xac\xed\x00\x05t\x00）
-        // StringRedisSerializer | Jackson2JsonRedisSerializer<>(Object.class)
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
-        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
-
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
-
-}
+//package cn.ultronxr.remote.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//import java.net.UnknownHostException;
+//
+//
+//@Configuration
+//public class RedisConfig {
+//
+//    /**
+//     * 修改 RedisTemplate 配置
+//     */
+//    @Bean
+//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory)
+//            throws UnknownHostException {
+//        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(factory);
+//
+//        // 修改 RedisTemplate K-V序列化方式，避免存入redis时出现乱码（\xac\xed\x00\x05t\x00）
+//        // StringRedisSerializer | Jackson2JsonRedisSerializer<>(Object.class)
+//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//        redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
+//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+//        redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
+//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+//
+//        redisTemplate.afterPropertiesSet();
+//        return redisTemplate;
+//    }
+//
+//}
